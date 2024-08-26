a1cb3459749ff95deea43d47b16c329fc5fd7426
shameer.2001 <shameer.a959@gmail.com>
2024-08-26 19:45:16 +0100

    Code the `Wallet` class and complete the `Chain` class

--- crypto.js
+++ crypto.js
@@ -62,9 +62,38 @@ class Chain {
     get verylastBlock() {
         return this.chain[this.chain.length - 1];
     }
-    addBlock() {
+    addBlock(transaction, senderPublicKey, signature) {
+        // Verify signature, reciever etc (ie the transaction data) with the public key of sender
+        const verifier = crypto.createVerify('SHA256');
+        verifier.update(transaction.toString());
+        const isValid = verifier.verify(senderPublicKey, signature); // Verify
+        // Create new block if verified
+        if (isValid) {
+            const newBlock = new Block(this.verylastBlock.hash, transaction);
+            this.chain.push(newBlock); // Add block to `chain` array
+        }
     }
 }
 Chain.instance = new Chain(); // To ensure only 1 chain is created
+// Wallet gives a user a public/private keypair & tf allows users to send and receive
 class Wallet {
+    constructor() {
+        const generate_keys = crypto.generateKeyPairSync('rsa', {
+            modulusLength: 2048,
+            publicKeyEncoding: { type: 'spki', format: 'pem' },
+            privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
+        });
+        // Define our keys
+        this.publicKey = generate_keys.publicKey;
+        this.privateKey = generate_keys.privateKey;
+    }
+    sendMoney(amount, receiverPublicKey) {
+        const transaction = new Transaction(amount, this.publicKey, receiverPublicKey); // Send to receiver by using his public key
+        // Sign transaction with private key
+        const sign = crypto.createSign('SHA256');
+        sign.update(transaction.toString()).end();
+        const signature = sign.sign(this.privateKey); // Sign
+        // Add block which as this single transaction inside
+        Chain.instance.addBlock(transaction, this.publicKey, signature); // The SENDER's public key is to verify the signature, the reciever etc (ie the transaction)
+    }
 }
--- crypto.ts
+++ crypto.ts
@@ -53,14 +53,55 @@ class Chain {
         return this.chain[ this.chain.length -1 ];
     }
 
-    addBlock() {
+    addBlock(transaction: Transaction, senderPublicKey: string, signature: Buffer) {
+
+        // Verify signature, reciever etc (ie the transaction data) with the public key of sender
+        const verifier = crypto.createVerify('SHA256');
+        verifier.update(transaction.toString());
+        const isValid = verifier.verify(senderPublicKey, signature); // Verify
+
+        // Create new block if verified
+        if (isValid) {
+            const newBlock = new Block(this.verylastBlock.hash, transaction);
+            this.chain.push(newBlock); // Add block to `chain` array
+        }
 
     }
 
-
 }
 
+// Wallet gives a user a public/private keypair & tf allows users to send and receive
 class Wallet {
+    public publicKey: string;
+    public privateKey: string;
+
+
+    constructor() {
+        const generate_keys = crypto.generateKeyPairSync('rsa', {
+            modulusLength: 2048, // Length of key
+            publicKeyEncoding: { type: 'spki', format: 'pem' },
+            privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
+          }
+        );
+        
+        // Define our keys
+        this.publicKey = generate_keys.publicKey
+        this.privateKey = generate_keys.privateKey
+    }
+
+
+    sendMoney(amount: number, receiverPublicKey: string) {
+        const transaction = new Transaction(amount, this.publicKey, receiverPublicKey); // Send to receiver by using his public key
+
+        // Sign transaction with private key
+        const sign = crypto.createSign('SHA256');
+        sign.update(transaction.toString()).end();
+        const signature = sign.sign(this.privateKey); // Sign
+        
+        // Add block which as this single transaction inside
+        Chain.instance.addBlock(transaction, this.publicKey, signature); // The SENDER's public key is to verify the signature, the reciever etc (ie the transaction)
+
+    }
 
 }
 
